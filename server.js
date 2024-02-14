02879b200f81d66c33dad4b780cf26ce7f56caef
Songpob <jsongpob@gmail.com>
2024-02-15 02:21:22 +0700

    Can connection with Unity from Controller Client

--- server.js
+++ server.js
@@ -7,22 +7,13 @@ var io = require('socket.io')(http, {
 });
 var express = require('express')
 
+//DIRECT TO INDEX.HTML
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 })
 
-io.on('connection', (socket) => {
-    console.log(`+ Device ${socket.id} connected`);
 
-    socket.on('disconnect', () => {
-        console.log(`- ${socket.id} disconnected`);
-    });
-
-    socket.on('chat message', (msg) => {
-        console.log('message: ' + msg);
-      });
-});
-
+//THROW ERROR WHEN GET SOME ERROR
 io.engine.on("connection_error", (err) => {
     console.log(err.req);      // the request object
     console.log(err.code);     // the error code, for example 1
@@ -30,6 +21,29 @@ io.engine.on("connection_error", (err) => {
     console.log(err.context);  // some additional error context
 });
 
-http.listen(80, function () {
+//LISTEN FROM PORT?
+http.listen(3000, function () {
     console.log('listening on *:3000');
+});
+
+
+//SOCKET ON WITH 'CHANNEL' WHEN OPEN 'CONNECTION'
+io.on('connection', (socket) => {
+    console.log(`+ Device ${socket.id} connected`); // connection devices
+
+    //WHEN CLIENT DISCONNECT FROM SOCKET
+    socket.on('disconnect', () => {
+        console.log(`- ${socket.id} disconnected`);
+    });
+
+    //CHANNELS SOCKET
+    //RECEIVE FROM CLIENT SOCKET ON 'channel_platfrom' CHANNEL
+    socket.on('channel_platfrom', (data) => {
+        // console.log('channel_platfrom: ' + data);
+
+        if (data == "connect") {
+            socket.emit('client', 'hello client'); //SENT BACK TO CLIENT ON 'client' CHANNEL
+            socket.broadcast.emit('test', 'world'); //BROADCAST TO ALL CLIENT OPEN WITH 'test'
+        }
+    });
 });
